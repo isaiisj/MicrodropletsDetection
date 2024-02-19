47d0fcf4f51ac711f57dc0735c2b0baef925fb8b
Isai <47548422+isaiisj@users.noreply.github.com>
2024-02-18 23:49:54 -0600

    Update od2.py 18/02/2024
    Se creo una interfaz que ya usa raton y se implementaron un procesamient para imagen mejorada y sin mejorar.

--- od2.py
+++ od2.py
@@ -9,108 +9,95 @@ from tkinter.messagebox import showinfo
 from scipy import fftpack
 from PIL import Image
 
+####Function that creates a dialog box to show the number of droplets###
 def show_variable(variable_value):
     root = tk.Tk()
     root.withdraw()  # Hide the main window
 
     showinfo("Total droplets",f"Number of droplets {variable_value}")
 
-
-
-def getInput(): 
-    inp = inputtxt.get(1.0, "end-1c") 
-    #lbl.config(text = "Provided Input: "+inp) 
-    return inp
-
-def setPath():
-    global path
-    path = getInput()
-    frame.destroy()
-
-# Top level window 
-frame = tk.Tk() 
-frame.title("Introduzca imagen") 
-frame.geometry('400x200') 
-# Function for getting Input 
-# from textbox and printing it  
-# at label widget 
-
-inputtxt = tk.Text(frame, height=5, width=20)
-inputtxt.pack()
-
-printButton = tk.Button(frame, text="Scan", command=setPath)
-printButton.pack()
-
-
+########################################
 '''
-# Get the current working directory (directory where the script is located)
-script_directory = os.path.dirname(os.path.abspath(__file__))
+class CanvasImage:
+    def __init__(self, title = "Image Loader"):
+        self.master = tk.Tk()
+        self.master.withdraw()
+        self.master.title(title)
+        self.canvas = tk.Canvas(self.master)
+        self.canvas.grid(row = 0, column = 0, sticky = tk.NSEW)
+        self.image_button = tk.Button(
+            self.master, font = "Helvetica 12",
+            text = "Choose Image", command = self.choose_image)
+        self.image_button.grid(row = 1, column = 0, sticky = tk.NSEW)
+        self.master.update()
+        self.master.resizable(False, False)
+        self.master.deiconify()
+        self.image_path = None  # Variable to store the image path
+    def choose_image(self):
+        image_name = fd.askopenfilename(title = "Pick your image")
+        print(image_name)
+        if image_name:
+            self.image_path = image_name  # Save the image path to the variable
+            self.image = tk.PhotoImage(file = image_name, master = self.master)
+            w, h = self.image.width(),self.image.height()
+            self.canvas.config(width = w, height = h)
+            self.canvas.create_image((0,0), image = self.image, anchor = tk.NW)
+        return self.image_path '''
+'''
+if __name__ == "__main__":
+    loader = CanvasImage()
+    loader.master.mainloop()'''
+'''
+loader = CanvasImage()
+path = loader.choose_image()
+loader.master.mainloop()'''
+###########################################################33
 
-# Top level window 
-frame = tk.Tk() 
-frame.title("Introduzca imagen") 
-frame.geometry('400x200') 
-# Function for getting Input 
-# from textbox and printing it  
-# at label widget 
+import tkinter as tk
+from tkinter import filedialog as fd
 
-def select_file():
-    global path
+class ImageSelectorApp:
 
-    filetypes = (
-        ('Image files', '*.jpg'),
-        ('Image files', '*.JPG'),
-        ('All files', '*.*')
-    )
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Image Selector")
+        self.master.resizable(False, False)
 
+        self.label = tk.Label(master, text="Choose Image:")
+        self.label.pack()
 
-    path = fd.askopenfilename(
-        title='Open a file',
-        initialdir=script_directory,
-        filetypes=filetypes)
+        self.image_path_var = tk.StringVar()
+        self.image_path_label = tk.Label(master, textvariable=self.image_path_var)
+        self.image_path_label.pack()
 
-    showinfo(
-        title='Selected File',
-        message=path
-    )
+        self.select_button = tk.Button(master, text="Select Image", command=self.select_image,font="Helvetica 12", width=35, height=3)
+        #self.select_button.pack()
+        self.select_button.pack(pady=10)
 
-    if path:
-        frame.destroy()
+        self.selected_image_path = None
 
+        #self.selected_image_paths = []
 
-# open button
-open_button = ttk.Button(
-    frame,
-    text='Open a File',
-    command=select_file
-)
-
-open_button.pack(expand=True)
+    def select_image(self):
+        image_path = fd.askopenfilename(title="Select an image", filetypes=[("Image Files", "*.png *.jpg *.jpeg *.gif")])
+        if image_path:
+            relative_path = os.path.relpath(image_path)
+            self.image_path_var.set(relative_path)
+            self.selected_image_path = relative_path
 
 
-# run the application
-#frame.mainloop()
+#if __name__ == "__main__":
+root = tk.Tk()
+app = ImageSelectorApp(root)
+root.mainloop()
 
-
+path = app.selected_image_path
 if path:
-    # Make the path relative to the script's directory
-    relative_path = os.path.relpath(path, script_directory)
+    print("Selected image path:", path)
+else:
+    print("No image selected.")
 
-    # Remove leading and trailing whitespace
-    trimmed_path = relative_path.strip()
-
-    print("Script Directory:", script_directory)
-    print("Relative Path:",relative_path)
-'''
-
-
-path = " "
-
-# run the application
-frame.mainloop()
-
-# Now, you can use the 'path' variable after the mainloop, and it will be updated when the "Scan" button is clicked.
-print(path)
+########################################################################
 
 #Read image
 img = cv2.imread(path, cv2.IMREAD_COLOR)
@@ -125,7 +112,6 @@ cv2.imshow("resized", copia)
 cv2.waitKey(0)
 
 
-
 #Separamos en 3 canales y Aumentamos contrasted del canal verde
 #RGB split
 b, g , r  = cv2.split(copia)
@@ -133,52 +119,34 @@ enhanced_green = cv2.equalizeHist(g)
 # Merge the channels back
 enhanced_image = cv2.merge([b, enhanced_green, r])
 # Display the original and enhanced images
-cv2.imshow('Enhanced Image', enhanced_image)
-cv2.waitKey(0)
+#cv2.imshow('Enhanced Image', enhanced_image)
+#cv2.waitKey(0)
 
-#############################################
 
 #Thresholding
 _,th = cv2.threshold(enhanced_green,150,255, cv2.THRESH_BINARY)
-cv2.imshow("binary", th)
-cv2.waitKey(0)
-
-#Umbralizacion de pixeles
-'''
-_, thresh1 = cv2.threshold(g2, 79, 255, cv2.THRESH_BINARY_INV)
-thresh1 = cv2.medianBlur(thresh1,5,0)
-thresh1 = cv2.Canny(thresh1, 10, 60)
-thresh1 = cv2.dilate(thresh1, (3,3), iterations=1)
-'''
-
-#cv2.imshow('Binary Threshold', thresh1)
-
-'''hist = cv2.calcHist([g2], [0], None, [256], [0, 256])
-plt.plot(hist, color='gray' )
-
-plt.xlabel('intensidad de iluminacion')
-plt.ylabel('cantidad de pixeles')
-plt.show()'''
+#cv2.imshow("binary", th)
+#cv2.waitKey(0)
 
 
 #Bluring image
 g2 = cv2.medianBlur(th,3)
 #g2 = cv2.medianBlur(th,5)
-cv2.imshow("blurred", g2)
-cv2.waitKey(0)
+#cv2.imshow("blurred", g2)
+#cv2.waitKey(0)
 
 
 #Border detection
 g2 = cv2.Canny(g2,0,10)
 #g2 = cv2.Canny(g2,0,20)
-cv2.imshow("canny", g2)
-cv2.waitKey(0)
+#cv2.imshow("canny", g2)
+#cv2.waitKey(0)
 
 #Border dilation and erode
 g2 = cv2.dilate(g2, None, iterations=1)
 g2 = cv2.erode(g2, None, iterations=1)
-cv2.imshow("dilate and erode", g2)
-cv2.waitKey(0)
+#cv2.imshow("dilate and erode", g2)
+#cv2.waitKey(0)
 
 
 #rows = g2.shape[0]
@@ -211,14 +179,86 @@ if detected_circles is not None and detected_circles2 is not None:
 
         cv2.imshow("l",copia)'''
 
+ 
+############################################################################
+
+#Read image
+img2 = cv2.imread(path, cv2.IMREAD_COLOR)
+
+#Rotate image
+img3 = cv2.rotate(img2, cv2.ROTATE_90_CLOCKWISE)
+
+#Resizing image
+copia2 = img3
+copia2 = cv2.resize(copia,(1000,700),interpolation = cv2.INTER_AREA)
+cv2.imshow("resized", copia2)
+cv2.waitKey(0)
 
 
-###########################################################################
+#Separamos en 3 canales y Aumentamos contrasted del canal verde
+#RGB split
+b, g , r  = cv2.split(copia2)
+#enhanced_green = cv2.equalizeHist(g)
+# Merge the channels back
+#enhanced_image = cv2.merge([b, enhanced_green, r])
+# Display the original and enhanced images
+#cv2.imshow('Enhanced Image', enhanced_image)
+#cv2.waitKey(0)
 
+
+#Thresholding
+_,th = cv2.threshold(g,150,255, cv2.THRESH_BINARY)
+#cv2.imshow("binary", th)
+#cv2.waitKey(0)
+
+
+#Bluring image
+g2 = cv2.medianBlur(th,3)
+#g2 = cv2.medianBlur(th,5)
+#cv2.imshow("blurred", g2)
+#cv2.waitKey(0)
+
+
+#Border detection
+g2 = cv2.Canny(g2,0,10)
+#g2 = cv2.Canny(g2,0,20)
+#cv2.imshow("canny", g2)
+#cv2.waitKey(0)
+
+#Border dilation and erode
+g2 = cv2.dilate(g2, None, iterations=1)
+g2 = cv2.erode(g2, None, iterations=1)
+#cv2.imshow("dilate and erode", g2)
+#cv2.waitKey(0)
+
+
+#rows = g2.shape[0]
+detected_circles = cv2.HoughCircles(g2,cv2.HOUGH_GRADIENT, 1, 20, param1 = 100, param2 = 6, minRadius = 5, maxRadius = 12)
+#detected_circles = cv2.HoughCircles(g2,cv2.HOUGH_GRADIENT, 1, 20, param1 = 100, param2 = 11, minRadius = 13, maxRadius = 29)
+detected_circles2 = cv2.HoughCircles(g2,cv2.HOUGH_GRADIENT, 1, 20, param1 = 100, param2  = 30, minRadius = 3, maxRadius = 27)
+
+count2 = 0
+
+if detected_circles is not None and detected_circles2 is not None:
+
+    detected_circles = np.uint16(np.around(detected_circles))
+    detected_circles2 = np.uint16(np.around(detected_circles2))
+
+    for pt in detected_circles[0, :]:
+        a, b, r = pt[0], pt[1], pt[2]
+
+        cv2.circle(copia2, (a,b), r, (0, 255, 0), 2)
+
+        cv2.circle(copia2, (a,b), 1, (0, 0, 255), 3)
+        count2 += 1
+        cv2.imshow("Circles", copia2)
+
+############################################################################
 #Displaying image
 #cv2.imshow('image',copia)
 #cv2.imshow('Green', g2)
 #print(count)
 show_variable(count)
+sshow_variable(count2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
